cfd3517afb8f0d180b998b5cb3d48e2338d87596
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-05-18 12:33:28 +0530

    Update and rename Kadane's Algorithm to Kadane's Algorithm.py

--- Kadane's Algorithm.py
+++ Kadane's Algorithm.py
@@ -1,4 +1,4 @@
-Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
+'''Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.'''
 
 
 Example:
