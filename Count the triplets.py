606cfb2b3737edb70bdf537f81c36fb6632b4a54
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-05-18 12:36:05 +0530

    Update Count the triplets.py

--- Count the triplets.py
+++ Count the triplets.py
@@ -1,4 +1,4 @@
-'''Given an array Arr consisting of N distinct integers. The task is to count all the triplets such that sum of two elements equals the third element.'''
+'''Given an array Arr consisting of N distinct integers. The task is to count all the triplets such that sum of two elements equals the third element.
  
 Example 1:
 
@@ -7,7 +7,7 @@ N = 4
 arr[] = {1, 5, 3, 2}      
 Output: 2     
 Explanation: There are 2 triplets:     
- 1 + 2 = 3 and 3 +2 = 5
+ 1 + 2 = 3 and 3 +2 = 5 '''
 
 ----------------------------------
 
