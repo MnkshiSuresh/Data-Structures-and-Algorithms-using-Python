65885acba358f8da94b754a2e8ee3584cda0082c
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2024-05-18 12:34:18 +0530

    Update and rename Missing number in array to Missing number in array.py

--- Missing number in array.py
+++ Missing number in array.py
@@ -1,4 +1,4 @@
-Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+'''Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
 
 Example 1:
 
@@ -11,7 +11,7 @@ Example 2:
 Input:
 N = 10
 A[] = {6,1,2,8,3,4,7,10,5}
-Output: 9
+Output: 9  '''
 -------------------------------------------------------
 class Solution:
     def missingNumber(self, arr, N):
